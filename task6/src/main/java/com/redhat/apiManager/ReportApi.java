f9ec6fbf2d080ae72dcd2958b068dff164bd428f
Mohamed2Bahaa <119130296+Mohamed2Bahaa@users.noreply.github.com>
2023-05-22 18:45:48 +0300

    Communication API'S
    Classes that communicate with the POSTMAN API

---
+++ task6/src/main/java/com/redhat/apiManager/ReportApi.java
@@ -0,0 +1,22 @@
+package com.redhat.apiManager;
+
+public class ReportApi {
+    private int ownerId;
+    private int restaurantId;
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public int getRestaurantId() {
+        return restaurantId;
+    }
+
+    public void setRestaurantId(int restaurantId) {
+        this.restaurantId = restaurantId;
+    }
+}
